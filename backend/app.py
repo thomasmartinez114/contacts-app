dec0383b2f2606ac305eb8e690c776c7bee20e83
Thomas Martinez <devtommie114@gmail.com>
2024-07-11 00:27:36 -0400

    routes.py

--- backend/app.py
+++ backend/app.py
@@ -10,5 +10,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
+import routes
+
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ backend/routes.py
@@ -0,0 +1,10 @@
+from app import app, db
+from flask import request, jsonify
+from models import Contact
+
+# Get all contacts
+@app.route("/api/contacts", methods=["GET"])
+def get_contacts():
+    contacts = Contact.query.all()
+    result = [contact.to_json() for contact in contacts]
+    return jsonify(result)
