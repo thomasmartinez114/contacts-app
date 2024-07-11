3392f69c7baa8e512084d9f26f94ce252863f015
Thomas Martinez <devtommie114@gmail.com>
2024-07-11 00:57:55 -0400

    unable to create contact via postman

--- backend/routes.py
+++ backend/routes.py
@@ -3,8 +3,38 @@ from flask import request, jsonify
 from models import Contact
 
 # Get all contacts
-@app.route("/api/contacts", methods=["GET"])
+@app.route("/api/contacts",methods=["GET"])
 def get_contacts():
-    contacts = Contact.query.all()
-    result = [contact.to_json() for contact in contacts]
-    return jsonify(result)
+  contacts = Contact.query.all() 
+  result = [contact.to_json() for contact in contacts]
+  return jsonify(result)
+
+# Create a friend
+@app.route("/api/contacts",methods=["POST"])
+def create_contact():
+  try:
+    data = request.json
+
+    name = data.get("name")
+    role = data.get("role")
+    description = data.get("description")
+    gender = data.get("gender")
+
+    # Fetch avatar image based on gender
+    if gender == "male":
+      img_url = f"https://avatar.iran.liara.run/public/boy?username={name}"
+    elif gender == "female":
+      img_url = f"https://avatar.iran.liara.run/public/girl?username={name}"
+    else:
+      img_url = None
+
+    new_contact = Contact(name=name, role=role, description=description, gender= gender, img_url=img_url)
+
+    db.session.add(new_contact) 
+    db.session.commit()
+
+    return jsonify({"msg":"contact created successfully"}), 201
+    
+  except Exception as e:
+    db.session.rollback()
+    return jsonify({"error":str(e)}), 500
