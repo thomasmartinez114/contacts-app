6853c5add117bec354e974d88f56a32069bcc34d
Thomas Martinez <devtommie114@gmail.com>
2024-07-14 19:57:39 -0400

    added icons to navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,4 @@
+import { Container, Stack } from '@chakra-ui/react';
 import Navbar from './components/Navbar';
 
 function App() {
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,9 +1,9 @@
-import { Container, Box, Flex } from '@chakra-ui/react';
+import { Container, Box, Flex, Text } from '@chakra-ui/react';
 
 const Navbar = () => {
   return (
     <Container maxW={'900px'}>
-      <Box px={4} my={4} borderRadius={5}>
+      <Box px={4} my={4} borderRadius={5} bg={'gray.700'}>
         <Flex h='16' alignItems={'center'} justifyContent={'space-between'}>
           {/* Left side */}
           <Flex
@@ -11,7 +11,13 @@ const Navbar = () => {
             justifyContent={'center'}
             gap={3}
             display={{ base: 'none', sm: 'flex' }}
-          ></Flex>
+          >
+            <img src='/react.png' alt='react logo' width={50} height={50} />
+            <Text fontSize={'40px'}>+</Text>
+            <img src='/python.png' alt='python logo' width={50} height={40} />
+            <Text fontSize={'40px'}>=</Text>
+            <img src='/explode.png' alt='explode logo' width={50} height={50} />
+          </Flex>
 
           {/* Right side */}
           <Flex></Flex>
