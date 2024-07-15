cb2bf2e3f724f147e22d947f90a5397918d27bc1
Thomas Martinez <devtommie114@gmail.com>
2024-07-14 20:21:31 -0400

    dark/light mode colors change now by useColorModeValue()

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -5,6 +5,7 @@ import {
   Text,
   Button,
   useColorMode,
+  useColorModeValue,
 } from '@chakra-ui/react';
 import { IoMoon } from 'react-icons/io5';
 import { LuSun } from 'react-icons/lu';
@@ -13,7 +14,12 @@ const Navbar = () => {
   const { colorMode, toggleColorMode } = useColorMode();
   return (
     <Container maxW={'900px'}>
-      <Box px={4} my={4} borderRadius={5} bg={'gray.700'}>
+      <Box
+        px={4}
+        my={4}
+        borderRadius={5}
+        bg={useColorModeValue('gray.200', 'gray.700')}
+      >
         <Flex h='16' alignItems={'center'} justifyContent={'space-between'}>
           {/* Left side */}
           <Flex
