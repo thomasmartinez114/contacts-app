4cf26eb73dd1b63cf467a5a1c50f55ca3b4ed614
Thomas Martinez <devtommie114@gmail.com>
2024-07-14 20:59:58 -0400

    added responsiveness to grid layout

--- frontend/src/components/ContactGrid.jsx
+++ frontend/src/components/ContactGrid.jsx
@@ -1,7 +1,15 @@
-import React from 'react';
+import { Grid } from '@chakra-ui/react';
 
 const ContactGrid = () => {
-  return <div>ContactGrid</div>;
+  return (
+    <Grid
+      templateColumns={{
+        base: '1fr', // show 1 card on small screens
+        md: 'repeat(2, 1fr)', // show 2 on mediums
+        lg: 'repeat(3, 1fr)', // show 3 on large
+      }}
+    ></Grid>
+  );
 };
 
 export default ContactGrid;
