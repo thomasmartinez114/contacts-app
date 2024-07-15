d6444a80905b6763a25e54742b65d4927b9a9001
Thomas Martinez <devtommie114@gmail.com>
2024-07-14 20:43:51 -0400

    corrected code where createcontact was in contactgrid and vice versa

--- frontend/src/components/CreateContactModal.jsx
+++ frontend/src/components/CreateContactModal.jsx
@@ -1,7 +1,47 @@
 import React from 'react';
+import {
+  Button,
+  Flex,
+  FormControl,
+  FormLabel,
+  Input,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalHeader,
+  ModalOverlay,
+  useDisclosure,
+} from '@chakra-ui/react';
+import { BiAddToQueue } from 'react-icons/bi';
 
-const CreateContactModal = () => {
-  return <div>CreateContactModal</div>;
+const ContactGrid = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  return (
+    <>
+      <Button onClick={onOpen}>
+        <BiAddToQueue />
+      </Button>
+
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Create New Contact</ModalHeader>
+          <ModalCloseButton />
+
+          <ModalBody pb={6}>
+            <Flex alignItems={'center'} gap={4}>
+              <FormControl>
+                <FormLabel>Full Name</FormLabel>
+                <Input placeholder='John Doe' />
+              </FormControl>
+            </Flex>
+          </ModalBody>
+        </ModalContent>
+      </Modal>
+    </>
+  );
 };
 
-export default CreateContactModal;
+export default ContactGrid;
