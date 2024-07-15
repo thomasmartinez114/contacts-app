68a9315d418b0d5a8e73b10cd976f7bd11ce4f73
Thomas Martinez <devtommie114@gmail.com>
2024-07-14 20:27:34 -0400

    text with gradient coloring

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,28 @@
-import { Container, Stack } from '@chakra-ui/react';
+import { Container, Stack, Text } from '@chakra-ui/react';
 import Navbar from './components/Navbar';
 
 function App() {
   return (
     <Stack minH={'100vh'}>
       <Navbar />
-
-      <Container maxW={'1200px'} my={4}></Container>
+      <Container maxW={'1200px'} my={4}>
+        <Text
+          fontSize={{ base: '3xl', md: '50' }}
+          fontWeight={'bold'}
+          letterSpacing={'2px'}
+          textTransform={'uppercase'}
+          textAlign={'center'}
+          mb={8}
+        >
+          <Text
+            as={'span'}
+            bgGradient={'linear(to-r, cyan.400, blue.500)'}
+            bgClip={'text'}
+          >
+            My Contacts List
+          </Text>
+        </Text>
+      </Container>
     </Stack>
   );
 }
