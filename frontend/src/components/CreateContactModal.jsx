94ebb532369d24d3b84672dd864ef1a769e5fac3
Thomas Martinez <devtommie114@gmail.com>
2024-07-14 20:54:11 -0400

    created modal, added input fields, save/cancel button

--- frontend/src/components/CreateContactModal.jsx
+++ frontend/src/components/CreateContactModal.jsx
@@ -1,7 +1,83 @@
 import React from 'react';
+import {
+  Button,
+  Flex,
+  FormControl,
+  FormLabel,
+  Input,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalHeader,
+  ModalOverlay,
+  useDisclosure,
+  Textarea,
+  RadioGroup,
+  Radio,
+  ModalFooter,
+} from '@chakra-ui/react';
+import { BiAddToQueue } from 'react-icons/bi';
 
-const CreateContactModal = () => {
-  return <div>CreateContactModal</div>;
+const ContactGrid = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  return (
+    <>
+      <Button onClick={onOpen}>
+        <BiAddToQueue />
+      </Button>
+
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Create New Contact</ModalHeader>
+          <ModalCloseButton />
+
+          <ModalBody pb={6}>
+            <Flex alignItems={'center'} gap={4}>
+              {/* Full Name */}
+              <FormControl>
+                <FormLabel>Full Name</FormLabel>
+                <Input placeholder='John Doe' />
+              </FormControl>
+
+              {/* Role */}
+              <FormControl>
+                <FormLabel>Role</FormLabel>
+                <Input placeholder='Software Engineer' />
+              </FormControl>
+            </Flex>
+
+            {/* Description */}
+            <FormControl mt={4}>
+              <FormLabel>Description</FormLabel>
+              <Textarea
+                resize={'none'}
+                overflowY={'hidden'}
+                placeholder="He's a software engineer who loves to code and build things."
+              />
+            </FormControl>
+
+            {/* Gender Selection */}
+            <RadioGroup mt={4}>
+              <Flex gap={5}>
+                <Radio value='male'>Male</Radio>
+                <Radio value='female'>Female</Radio>
+              </Flex>
+            </RadioGroup>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme='blue' mr={3} onClick={onClose}>
+              Save
+            </Button>
+            <Button onClick={onClose}>Cancel</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
 };
 
-export default CreateContactModal;
+export default ContactGrid;
